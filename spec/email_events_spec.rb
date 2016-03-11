7e6f022bcef1b624e57ba226b5168e04cc09c44e
Kent Mewhort <kent@openissues.ca>
2016-03-11 13:02:53 -0800

    Clarify spec name.

--- spec/email_events_spec.rb
+++ spec/email_events_spec.rb
@@ -4,7 +4,7 @@ describe EmailEvents do
   context "Sendgrid is configured as the Rails email provider" do
     # this is the default set in the spec_helper
 
-    it "detects Sendgrid to use as the adapter" do
+    it "auto-detects Sendgrid to use as the adapter based on the smtp settings" do
       expect(EmailEvents.adapter).to eq EmailEvents::Adapters::Sendgrid
     end
   end
