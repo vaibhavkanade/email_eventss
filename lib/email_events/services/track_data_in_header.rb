80fb66f0121fc684c1086c3623f8e114f439e4b9
vaibhavkanade <vaibhavkanade@users.noreply.github.com>
2016-08-29 19:24:58 +0530

    Update track_data_in_header.rb

--- lib/email_events/services/track_data_in_header.rb
+++ lib/email_events/services/track_data_in_header.rb
@@ -11,15 +11,15 @@ class EmailEvents::Service::TrackDataInHeader < EmailEvents::Service
     # a UUID is much more secure identifier to send along in an email header (as opposed to the db primary key,
     # for which someone could easily guess a valid integer in an attack)
     uuid = generate_uuid
-
-    data_obj = sent_email_data_class.create!(
-      mailer_class: mailer_class,
-      mailer_action: mailer_action,
-      to: recipient_email,
-      uuid: uuid,
-      data: data
-    )
-
+    if recipient_email.present?
+      data_obj = sent_email_data_class.create!(
+        mailer_class: mailer_class,
+        mailer_action: mailer_action,
+        to: recipient_email,
+        uuid: uuid,
+        data: data
+      )
+    end
     # add the uuid to of the SentEmailData to the email Message-ID header for tracking it
     add_data_uuid_to_email_headers(uuid)
 
@@ -36,7 +36,7 @@ class EmailEvents::Service::TrackDataInHeader < EmailEvents::Service
   end
 
   def recipient_email
-    mailer.headers.to.first
+    mailer.headers.to.join(", ") rescue nil
   end
 
   def generate_uuid
